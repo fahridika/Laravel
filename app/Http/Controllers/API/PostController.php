b035568e4a880f38ab7719e2a07629f62cf4b0e2
Fahrikun <gunaon01@gmail.com>
2022-08-15 11:52:33 +0700

    2nd

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -6,11 +6,55 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 //Tambahan
-
+use App\Models\Post;
+use Validator;
 
 
 
 class PostController extends Controller
 {
-    //
+    //all  post
+    public function index()
+    {
+        $posts = Post::all()->toArray();
+    return array_reverse($posts);
+    }
+    //add post
+    public function add(Request $request)
+    {
+        $post =new post (
+            [
+                'title' => $request->input('title'),
+                'description' => $request->input('description')
+            ]
+            );
+            $post->save();
+ 
+    return response()->json('The post successfully added');
+    }
+    // edit post
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        return response()->json($post);
+    }
+    // update post
+    public function update($id, Request $request)
+    {
+        $post = Post::find($id);
+        $post->update($request->all());
+ 
+        return response()->json('The post successfully updated');
+    }
+
+    public function delete($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+ 
+        return response()->json('The post successfully deleted');
+    }
+    
+    
+
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('posts', 'PostController@index');
+Route::group(['prefix' => 'post'], function () {
+    Route::post('add', 'PostController@add');
+    Route::get('edit/{id}', 'PostController@edit');
+    Route::post('update/{id}', 'PostController@update');
+    Route::delete('delete/{id}', 'PostController@delete');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
