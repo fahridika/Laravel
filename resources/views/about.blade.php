84d3f2a278b55c09941f8608485951089a0c0769
Fahri <gunaon01@gmail.com>
2022-08-15 21:58:21 +0700

    route view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fahri | About</title>
+</head>
+<body>
+    <h1> Halaman home</h1>
+    <h3>Muhammad Fahri Dika Yuda</h3>
+    <p>gunaon01@gmail.com</p>
+    <img src="" alt="Fahri">
+<script src="js/script.js"></script>
+</body>
+</html>
