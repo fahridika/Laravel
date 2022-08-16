6524630c8f0b9dd29799478359e35ae4061e19b2
Fahrikun <gunaon01@gmail.com>
2022-08-16 09:10:39 +0700

    section dan extend

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fahri | About</title>
-</head>
-<body> 
+@extends('layout.main')
+@section('container')
     <h1> Halaman home</h1>
     <h3>{{$nama}}</h3>
     <p>{{$email}}</p>
     <img src="img/{{$image}}" alt="{{$nama}}" width='200'>
 <script src="js/script.js"></script>
-</body>
-</html>
+@endsection
