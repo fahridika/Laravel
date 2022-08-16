faefc378404b46019e8c4a6df0ee8f3fa53fdd2d
Fahrikun <gunaon01@gmail.com>
2022-08-16 08:59:14 +0700

    blade engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fahri | About</title>
 </head>
-<body>
-    <h1>  </h1>
-    <h3>Muhammad Fahri Dika Yuda</h3>
-    <p>gunaon01@gmail.com</p>
-    <img src="img/python.png" alt="Fahri" width='200'>
+<body> 
+    <h1> Halaman home</h1>
+    <h3>{{$nama}}</h3>
+    <p>{{$email}}</p>
+    <img src="img/{{$image}}" alt="{{$nama}}" width='200'>
 <script src="js/script.js"></script>
 </body>
 </html>
