1675729e67ffb5c2041f518d04ac30a916ec2827
Fahrikun <gunaon01@gmail.com>
2022-08-22 08:50:27 +0700

    Preparing db seeder

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,16 @@
 @extends('layout.main')
 @section('container')
-<h1> Halaman Blog</h1>
+<h1 class='mb-5'> Halaman Blog</h1>
+
 @foreach($posts as $post)
-<article>
+<article class='mb-5 border-bottom pb-5'>
     <h2>
-        <a href='/posts/{{ $post->slug}}'>{{ $post->title }}</a>
+        <a href='/posts/{{ $post->slug}}' class='text-decoration-none'>{{ $post->title }}</a>
     </h2>
-    <h3>{{ $post->author}}</h3>
-    {{ $post->excerpt }}
+    <p>By. Muhammad Fahri Dika Yuda in <a href='/category/{{ $post->category->slug }}' class='text-decoration-none'>{{ $post->category->name}}</a></p>
+
+    <p>{{ $post->excerpt }}</p>
+    <a  href='/posts/{{ $post->slug}}' class='text-decoration-none'> Read more...</a>
 </article>
 
 
